6cbef5431e1693615b23d68482fd2c0182a82ffd
Burak Kalafat <kalafatburak@gmail.com>
2019-05-11 13:18:46 +0300

    feat: Add all components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, {
-  Component
-} from 'react';
+import React from 'react'
+import ConnectedTodos from './Todos'
+import ConnectedGoals from './Goals'
+import { connect } from 'react-redux'
+import {
+  handleInitialData
+} from '../actions/shared'
 
-class App extends Component {
+class App extends React.Component {
+  componentDidMount () {
+    const { store } = this.props
+
+    store.dispatch(handleInitialData())
+    store.subscribe(() => this.forceUpdate())
+  }
   render() {
+    const { store } = this.props
+    const { todos, goals, loading } = store.getState()
+    if (loading === true) {
+      return <h3>Loading</h3>
+    }
     return (
-    <div>Hello World!</div>
-    );
+      <div>
+        <ConnectedTodos todos={todos} store={this.props.store} />
+        <ConnectedGoals goals={goals} store={this.props.store} />
+      </div>
+    )
   }
 }
-
-export default App;
+export default connect((
+  state) => ({
+    loading: state.loading
+  }))(App)
